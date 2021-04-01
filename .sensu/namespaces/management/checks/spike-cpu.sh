304a867b760c5cc8846644eaf13531c1d8295e87
victorgreentechshur <victor.green@techsur.solutions>
2021-04-01 06:31:43 -0400

    updated spec 1

---
+++ .sensu/namespaces/management/checks/spike-cpu.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+fulload() { dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null | dd if=/dev/zero of=/dev/null & }; fulload; read; pkill dd
